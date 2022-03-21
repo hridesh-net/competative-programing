79ccb38cffebc57bc4a4294125ff651a7014ef31
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2022-03-21 21:44:33 +0530

    pattern making

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************
+            pattern making
+
+                                1 2 3 4 5
+                                10 9 8 7 6 
+                                11 12 13 14 15 
+                                20 19 18 17 16 
+
+            take number 'N' as input and print N lines of the pattern
+*************************************************************************** */
+
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
