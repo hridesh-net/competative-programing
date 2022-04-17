93e8f52353a93cc16e6fa2f0d36b5ed201b0c668
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2022-04-17 15:49:06 +0530

    dicimal to binary code

---
+++ q1/decimal_to_binary.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int i = 0;
+
+
+    while (n != 0){
+
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        // ans = a + ans;
+
+        cout << n <<"|" << ans <<"|" << bit << endl;
+        n = n >> 1;
+        i++;
+
+    }
+
+    /*while (n != 0){
+
+        int bit = n % 2;
+        ans = (bit * pow(10, i)) + ans;
+
+        n = n/2;
+        i++;
+
+    }*/
+
+    cout << "binary of digit is " << ans;
+
+    return 0;
+}
